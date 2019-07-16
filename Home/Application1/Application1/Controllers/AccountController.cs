a712ed8c33b7ff62945ac882adf7c1afd91e5950
Kamal Guliyev <kamalag@code.edu.az>
2019-07-17 03:49:59 +0400

    *** Identity practice (register a new user) ***

---
+++ Home/Application1/Application1/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application1.Database;
+using Application1.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly MySample _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountController(MySample context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        [ActionName("Register")]
+        public IActionResult RegisterGet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Register")]
+        public async Task<IActionResult> RegisterPost(RegisterVM registerVM)
+        {
+            if(ModelState.IsValid)
+            {
+                MyUser newUser = new MyUser()
+                {
+                    FirstName = registerVM.FirstName,
+                    LastName = registerVM.LastName,
+                    Age = registerVM.Age,
+                    UserName = registerVM.Username,
+                    Email = registerVM.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+                if(!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Home");
+            //return View();
+        }
+    }
+}
---
+++ P110_Identity/P110_Identity/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using P110_Identity.DAL;
+using P110_Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using P110_Identity.Models;
+
+namespace P110_Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(AppDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Register()
+        {
+            ViewBag.Countries = _context.Countries;
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Countries = _context.Countries;
+                return View(registerViewModel);
+            }
+
+            ApplicationUser newUser = new ApplicationUser
+            {
+                Firstname = registerViewModel.Firstname,
+                Lastname = registerViewModel.Lastname,
+                Email = registerViewModel.Email,
+                UserName = registerViewModel.Username,
+                CityId = registerViewModel.CityId
+            };
+
+            IdentityResult identityResult = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+            if(!identityResult.Succeeded)
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                ViewBag.Countries = _context.Countries;
+                return View(registerViewModel);
+            }
+
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
